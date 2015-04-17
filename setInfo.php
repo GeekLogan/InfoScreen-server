c168dbb8f2f1c0c0ca3497e14e1ae13e4465fde3
GeekLogan <logan.walker@me.com>
2015-04-17 18:45:04 -0400

    Manually Merge SetInfo.php from local testing brach
    The code now actually allows setting of values from GET requests.

--- setInfo.php
+++ setInfo.php
@@ -1,5 +1,23 @@
 <?php
-    require 'writeToFile.php';
 
-    //TODO everything
+require 'std.php';
+
+$dom=new DOMDocument();
+
+$doc->formatOutput = false;
+$dom->load($STORE_FILE_NAME);
+$root=$dom->documentElement;
+
+$cmd = $_GET['cmd'];
+if($cmd == 'dismiss'){
+        $dom->getElementsByTagName('alertIsActive')->item(0)->nodeValue = 'false';
+} else if ($cmd == 'enable') {
+        $dom->getElementsByTagName('alertIsActive')->item(0)->nodeValue = 'true';
+        $dom->getElementsByTagName('alertText')->item(0)->nodeValue = $_GET['msg'];
+        $dom->getElementsByTagName('alertSender')->item(0)->nodeValue = $_GET['sender'];
+}
+
+$dom->save($STORE_FILE_NAME);
+echo $dom->saveXML();
+
 ?>
