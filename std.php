117b1a2db1e9f718980bb0d75d843d3249dadfc8
GeekLogan <logan.walker@me.com>
2015-04-17 18:54:34 -0400

    Add bad security fix

--- std.php
+++ std.php
@@ -6,4 +6,8 @@
     $BUSTIME_KEY = "";
     $BUSTIME_URL = "http://trip.osu.edu/bustime/api/v1/";
     $REQUEST_AUTH_TOKEN = ""; //Add value to require ?auth=xxx for valid requests
+    
+    if($REQUEST_AUTH_TOKEN != "" && $_GET['auth'] != $REQUEST_AUTH_TOKEN) {
+        die("You must authenticate with a propper token to access this server!");
+    }
 ?>
