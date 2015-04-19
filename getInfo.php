deb44df69f433e1b29755cadacb6fc6b2e19f229
Logan Walker <logan.walker@me.com>
2015-04-19 11:16:24 -0400

    Remove Blank Space in getInfo.php

--- getInfo.php
+++ getInfo.php
@@ -8,19 +8,11 @@
     $outputString = file_get_contents($STORE_FILE_NAME);
     $outputString = str_replace('<?xml version="1.0"?>', "", $outputString);
 
-    //START WEATHER
     include 'modules/weather.php';
-    //END WEATHER
-
-    //@TODO get/print calendar
-
-    //START BUSTIME NOTE: Bustime Spanning Tree Search has Been Removed to Save Time
     include 'modules/bustime.php';
-    //END BUSTIME
 
     $outputString = xmlWrapper($outputString, "infoScreen");
-    $outputString = xmlWrapper($outputString, "wrapper");
-    //The "wrapper" is stripped off by the JSON compiler
+    $outputString = xmlWrapper($outputString, "wrapper"); //removed by JSON
 
     $outputTmp = simplexml_load_string($outputString) or die('{"error": "Cannot create object"}');
     $outputFinal = json_encode($outputTmp);
